36f110a7d692ab2d2029096d22070b3c2393c32b
Marlon_Figueroa <marlon.f.1993@gmail.com>
2021-12-08 20:41:22 -0600

    Mejora para documentar api

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const rpcMethods = require("./routes/api");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use("/api", rpcMethods);
 
-const port = process.env.PORT || 3000;
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Moda Guanaca API with Swagger",
+        version: "0.0.1",
+        description:
+          "Documentación con Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "Moda Guanaca - Comercio Eléctronico 2021",
+          url: "https://modaguanaca.com",
+          email: "marlon.f.993@gmail.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/modaguanaca",
+        },
+      ],
+    },
+    apis: ["./routes/api.js"],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
 
+const port = process.env.PORT || 3000;
 server = app.listen(port, () => console.log(`Servidor corriendo sobre puerto ${port}`));
